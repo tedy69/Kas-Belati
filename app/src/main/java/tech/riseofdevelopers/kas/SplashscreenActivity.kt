eb94dfbf217d0d64a37aafe787f2f4f43f8c0e12
tedy69 <tedyfazrin7@gmail.com>
2021-03-13 11:30:13 +0700

    pertemuan 4

---
+++ app/src/main/java/tech/riseofdevelopers/kas/SplashscreenActivity.kt
@@ -0,0 +1,11 @@
+package tech.riseofdevelopers.kas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashscreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
